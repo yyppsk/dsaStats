50c999ed1e18a599a1b1ce7894c3999338f7bd62
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-07-25 17:41:20 +0530

    Home Route Fix

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/api/codolio/:username", async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
