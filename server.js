bd9e9169ca33c6914deb4ab135dc436e8358c790
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-07-25 19:08:00 +0530

    Server Update

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-// server.js
 const express = require("express");
-const puppeteer = require("puppeteer");
-const app = express();
-
+const puppeteer = require("puppeteer-core");
+const chrome = require("chrome-aws-lambda");
 const path = require("path");
 const svgTemplate = require("./svgTemplate");
 
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "./frontend")));
@@ -13,8 +12,14 @@ app.use(express.static(path.join(__dirname, "./frontend")));
 app.get("/api/codolio/:username", async (req, res) => {
   console.log(req.originalUrl);
   const username = req.params.username;
+
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath,
+      headless: chrome.headless,
+    });
     const page = await browser.newPage();
     await page.goto(`https://codolio.com/profile/${username}`, {
       waitUntil: "networkidle2",
@@ -38,7 +43,6 @@ app.get("/api/codolio/:username", async (req, res) => {
     const svg = svgTemplate(username, totalQuestions, totalContests, awards);
 
     res.set("Content-Type", "image/svg+xml");
-    // res.set("Cache-Control", "no-cache"); // Disable caching
     res.send(svg);
   } catch (error) {
     console.error(error);
@@ -47,7 +51,7 @@ app.get("/api/codolio/:username", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/index.html"));
 });
 
 app.listen(port, () => {
