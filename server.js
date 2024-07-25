581cd48e0da3d6e6c9f9b6b75e0018ad0ba10852
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-07-25 20:18:03 +0530

    Playwright Update

--- server.js
+++ server.js
@@ -1,52 +1,41 @@
 const express = require("express");
-const puppeteer = require("puppeteer-core");
-const chrome = require("chrome-aws-lambda");
+const { chromium } = require("playwright"); // Import Playwright
+const app = express();
 const path = require("path");
 const svgTemplate = require("./svgTemplate");
 
-const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "./frontend")));
 
 app.get("/api/codolio/:username", async (req, res) => {
+  console.log(req.originalUrl);
   const username = req.params.username;
   try {
-    // Puppeteer launch options
-    const options = {
-      args: chrome.args,
-      executablePath: await chrome.executablePath,
-      headless: chrome.headless,
-      ignoreDefaultArgs: ["--disable-extensions"],
-    };
-
-    const browser = await puppeteer.launch(options);
+    // Launch a new browser instance
+    const browser = await chromium.launch();
     const page = await browser.newPage();
+
     await page.goto(`https://codolio.com/profile/${username}`, {
-      waitUntil: "networkidle2",
+      waitUntil: "networkidle",
     });
 
-    const totalQuestionsText = await page.$eval(
-      "#total_questions",
-      (el) => el.textContent
-    );
+    const totalQuestionsText = await page.textContent("#total_questions");
     const totalQuestions = parseInt(totalQuestionsText.match(/\d+/)[0], 10);
 
-    const totalContests = await page.$eval(
-      "#contest_description span",
-      (el) => el.textContent
-    );
-    const awards = await page.$eval("#badges div span", (el) => el.textContent);
+    const totalContests = await page.textContent("#contest_description span");
+    const awards = await page.textContent("#badges div span");
 
     await browser.close();
 
     const svg = svgTemplate(username, totalQuestions, totalContests, awards);
 
     res.set("Content-Type", "image/svg+xml");
+    // res.set("Cache-Control", "no-cache"); // Disable caching
     res.send(svg);
   } catch (error) {
     console.error(error);
-    res.status(500).send("User not found or error fetching data");
+    res.status(404).send("User not found or error fetching data");
   }
 });
 
