e2c50b8b1d208d621b1756b473eeba4c4f48e10e
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-07-25 17:19:52 +0530

    BuildJS - Syntax Error Fix

--- scripts/build.js
+++ scripts/build.js
@@ -3,6 +3,6 @@ const path = require("path");
 
 // Example of copying a static HTML file to the dist folder
 fs.copyFileSync(
-  path.join(__dirname, "../frontend/index.html"),
-  path.join(__dirname, "../dist/index.html")
+  path.join(__dirname, "./frontend/index.html"),
+  path.join(__dirname, "./dist/index.html")
 );
